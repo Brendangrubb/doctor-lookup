c81fb3e77cd7e9c4cc752a2e220d9c8535ceed33
Brendan Grubb <brendangrubb@gmail.com>
2017-03-17 15:25:18 -0700

    add and parse phone number

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -5,25 +5,28 @@ function displayDoctors(result) {
   for (i = 0; i < result.data.length; i++)
   {
     console.log(result.data[i]);
-
+    //star rating image parser
     if (result.data[i].ratings.length === 0 || result.data[i].ratings[0] === 'undefined') {
       var rating_image = "No rating available";
     } else {
       var rating_image = "<img src='" + result.data[i].ratings[0].image_url_small + "' alt = 'rating'>";
     }
-
+    //new patient parser
     if (result.data[i].practices[0].accepts_new_patients === true) {
       var new_patients = "Absolutely!";
     } else {
       var new_patients = "Not at the moment.";
     }
-
+    //website image parser
     if (result.data[i].practices[0].website === undefined) {
-      var website = result.data[i].practices[0].website;
+      var website = "no website given...google them!";
     } else {
       var website = "<a href='" + result.data[i].practices[0].website + "'>" + result.data[i].practices[0].website + "</a>";
     }
-
+    //phone number parser
+    var raw_phone = result.data[i].practices[0].phones[0].number
+    var phone_number = "(" + raw_phone.substr(0,3) + ")" + raw_phone.substr(3,3) + "-" + raw_phone.substr(6,4);
+    //add data to form
     $("#results").append
     (
       "<tr>" +
@@ -31,9 +34,11 @@ function displayDoctors(result) {
         "<td>" + result.data[i].profile.first_name + result.data[i].profile.last_name + ", " + result.data[i].profile.title + "</td>" +
         "<td>" + rating_image + "</td>" +
         "<td>" + new_patients + "</td>" +
+        "<td>" + phone_number + "</td>" +
         "<td>" + website + "</td>" +
       "</tr>"
     );
+  //end for loop
   }
 
 }
@@ -47,7 +52,5 @@ $(document).ready(function() {
 
     doctor.getNames(ailment, displayDoctors);
   });
-
-
 
 });
--- js/doctor.js
+++ js/doctor.js
@@ -1,22 +1,14 @@
 var apiKey = require('./../.env').apiKey;
 
-
-function Doctor() {
-}
-
-
+function Doctor() {}
 
 Doctor.prototype.getNames = function (ailment, displayDoctors) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + ailment + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
   .then(function(result) {
     displayDoctors(result)
-  })
-  .fail(function(error){
+  }).fail(function(error){
     $('#results').append(  "<tr>" +  "<td>" + error.responseJSON.message + "</td>" + "</tr>");
   });
-
 };
-
-
 
 exports.doctorModule = Doctor;
